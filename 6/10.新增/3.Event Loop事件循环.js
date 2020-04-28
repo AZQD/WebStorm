91b13955d59bb0630af4db20a91f9ae501ad3b33
AZQD <604972395@qq.com>
2020-04-29 01:32:59 +0800

    Event Loop事件循环

---
+++ 6/10.新增/3.Event Loop事件循环.js
@@ -0,0 +1,59 @@
+// 参考：https://segmentfault.com/a/1190000016278115
+
+/*
+//demo01
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+  Promise.resolve().then(() => {
+    console.log(3)
+  });
+});
+
+new Promise((resolve, reject) => {
+  console.log(4)
+  resolve(5)
+}).then((data) => {
+  console.log(data);
+})
+
+setTimeout(() => {
+  console.log(6);
+})
+
+console.log(7);*/
+
+
+//demo02
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+  Promise.resolve().then(() => {
+    console.log(3)
+  });
+});
+
+new Promise((resolve, reject) => {
+  console.log(4)
+  resolve(5)
+}).then((data) => {
+  console.log(data);
+
+  Promise.resolve().then(() => {
+    console.log(6)
+  }).then(() => {
+    console.log(7)
+
+    setTimeout(() => {
+      console.log(8)
+    }, 0);
+  });
+})
+
+setTimeout(() => {
+  console.log(9);
+})
+
+console.log(10);
