a07f8c97060e96ecd5e5ca89889bc939f1937e3f
AZQD <604972395@qq.com>
2021-01-19 20:38:14 +0800

    文件格式互转

---
+++ 6/10.新增/30.文件格式互转-Node.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const path = require('path');
+
+//获取当前图片的格式
+function getImageType (str) {
+    var reg = /\.(png|jpg|gif|jpeg|webp)$/;
+    return str.match(reg)[1];
+}
+
+let fileName = '30.test.png'; // 文件名称
+let dirPath = path.join(__dirname, fileName); // 本地路径名称
+fs.readFile(dirPath, function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        const buffer = new Buffer(data, 'binary');
+        const base64Data = 'data: image/' + getImageType(fileName) + ';base64,' + buffer.toString('base64');
+        console.log('base64Data：', base64Data);
+    }
+});
